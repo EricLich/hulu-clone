4fffd3c82cf9558f7655ee685e764f3db1eadfbd
Eric Lich <eric@Erics-Air.fibertel.com.ar>
2021-07-11 18:19:14 -0300

    basic front-end with api calls ready

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import HeaderItem from './HeaderItem'
 import {
-    BaddgeCheckIcon,
+    BadgeCheckIcon,
     CollectionIcon,
     HomeIcon,
     LightningBoltIcon,
@@ -11,9 +11,14 @@ import {
 
 const Header = () => {
     return (
-        <header>
-            <div>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly align-middle max-w-2xl">
                 <HeaderItem title='HOME' Icon={HomeIcon}/>
+                <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
+                <HeaderItem title='COLECCIONT' Icon={CollectionIcon}/>
+                <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+                <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
             </div>
             <Image 
                 className="object-contain"
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,35 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+  const req = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`)
+                        .then(res => res.json())
+                        .catch(err => console.log(err))
+  return{
+    props:{
+      results: req.results
+    }
+  }
+}
+
+export default function Home({results}) {
   return (
-
     <div>
       <Head>
         <title>Hulu Clone</title>
         <meta name="keywords" content="react, clone, hulu, video, next.js, web, js, javascript" />
       </Head>
-
       {/* Header */}
       <Header/>
       {/* Nav */}
+      <Nav/>
       {/* Results */}
-      
+      <Results results={results}/>
     </div>
   )
 }
