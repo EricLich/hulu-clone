d16afde5a7bc324622de0c9429785cb42b508fa3
Eric Lich <eric@Erics-Air.fibertel.com.ar>
2021-07-10 22:08:47 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+
+    <div>
+      <Head>
+        <title>Hulu Clone</title>
+        <meta name="keywords" content="react, clone, hulu, video, next.js, web, js, javascript" />
+      </Head>
+
+      {/* Header */}
+      <Header/>
+      {/* Nav */}
+      {/* Results */}
+      
+    </div>
+  )
+}
